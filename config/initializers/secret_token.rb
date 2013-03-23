054e0adc999dc5dbdfe78baaadfcdae9ace6778a
Carlos Veliz <cxrlos@gmail.com>
2013-03-22 20:34:13 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cx::Application.config.secret_token = 'ece73bbc62cff2b1711d96103a5808ab20399b517c431c289b9b9e5709e3c585bb1044e3fb37d486063e425b31dc9a90f0ffae5fce00875655f3526e1c847ef3'
